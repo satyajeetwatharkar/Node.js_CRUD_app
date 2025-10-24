b3f66efd183187296e5604b76decf8d9a2946deb
Satyajeet <satyajeetwatharkar@gmail.com>
2025-10-23 23:16:15 -0400

    23rd oct

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const dotenv= require("dotenv").config();
 
 const app = express();
 
-const port=process.env.PORT || 5000;
+const port=process.env.PORT;
+
+app.get("/api/contacts",(req,res) => {
+    res.status(200).json({message:"Get all contacts"});
+});
 
 app.listen(port,()=>{
-    console.log(`Server running on ${port}`);
+    console.log(`Server running on port ${port}`);
 });
