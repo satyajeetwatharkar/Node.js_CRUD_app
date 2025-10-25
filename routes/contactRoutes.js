d7c91d2611d0b0d5a3cb045d3011bd52fd715409
Satyajeet <satyajeetwatharkar@gmail.com>
2025-10-24 23:14:00 -0400

    24 Oct

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {getContact, createContact,getContacts,updateContact, deleteContact} = require("../Controllers/contactController");
 
+router.route("/").get(getContacts);
+
+router.route("/").post(createContact);
+
+router.route("/:id").get(getContact);
+
+router.route("/:id").put(updateContact);
+
+router.route("/:id").delete(deleteContact);
+
+module.exports = router;
